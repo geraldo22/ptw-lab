107034da711dedc1a771d1f5e41a609126d47f26
geraldo <pedro.geraldo@ua.pt>
2017-03-17 00:52:28 +0000

    trab6 completo

--- trab6/calculadora.js
+++ trab6/calculadora.js
@@ -1,18 +1,39 @@
+var keys = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "C", "0", "=", "+"];
+
+var num = prompt("Insira o numero de teclas", "16")
+
+var j = 0;
+for (var i = 0; i < num; i++)
+{
+    var newKey = document.createElement("div");
+    var keyV = document.createTextNode(keys[j]);
+    newKey.appendChild(keyV);
+
+    var newKeyAtt = document.createAttribute("class");
+    newKeyAtt.value = "key";
+    newKey.setAttributeNode(newKeyAtt);
+
+    var getKeypad = document.getElementById("keypad");
+    getKeypad.appendChild(newKey);
+
+    if (j == keys.length - 1)
+        j = 0;
+    else
+        j++;
+}
+
 var key = document.getElementsByClassName("key");
 for (var i = 0; i < key.length; i++)
+{
     key[i].onclick = keyPress;
+}
+
 
 function keyPress()
 {
     var xp = document.getElementById("e").value;
     var kp = this.textContent;
     var lc = kp.charCodeAt((kp.length) - 1);
-    /*if(lc < 48 || lc > 57)
-    {
-        if()
-        kp.replace(kp.charAt((kp.length)-1), xp);
-    }
-    else*/
     if (kp == '=')
     {
         calcular();
@@ -23,20 +44,25 @@ function keyPress()
     }
     else
     {
-        document.getElementById("e").value += kp;
+        document.getElementById("e").value = xp.concat(kp);
     }
 }
 
 function calcular()
 {
-    var r = eval(document.getElementById("e").value);
-    if (r == null || r === "undefined" || isNAN(r) || !isFinite(r))
+    var exp = document.getElementById("e").value;
+    try
+    {
+        if (exp == "")
+            throw err;
+        var r = eval(exp);
+        if (r == "Infinity")
+            throw err;
+        document.getElementById("r").value = r;
+    }
+    catch (err)
     {
         document.getElementById("r").value = "expressão inválida";
-    }
-    else
-    {
-        document.getElementById("r").value = r;
     }
 }
 
